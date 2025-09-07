706f5d385516db7466e0340a9c53c99c002daee2
Kumari Shruti <kumarishruti10012005@gmail.com>
2025-09-07 16:39:57 +0530

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'portfolio',
         'USER':'root',
-        'PASSWORD':'ShrutI@20',
+        'PASSWORD':'*********',
         'HOST':'localhost',
         'PORT':3306,
     }
